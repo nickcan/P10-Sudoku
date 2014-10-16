7498eb3b9b010e2219cf72a633ffbcf50aceb4ef
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-16 09:43:22 -0700

    Shuffled somethings around in SudokuRazy.js so that the view initializes properly

--- public/js/SudokuRazy.js
+++ public/js/SudokuRazy.js
@@ -1,16 +1,18 @@
 SudokuRazy = {};
 
 SudokuRazy.app = function() {
-  // For the Welcome page and typing of welcome message
+  // Initial hiding of forms and other things in the view upon page load.
+  SudokuRazy.GameView.hideLightbox();
   SudokuRazy.WelcomeView.hideForms();
+  $('.incorrect').hide()
+  $('#tabs').tabs();
+
+  // For the Welcome page and typing of welcome message
   var title = "Welcome to SudokuRazy".split("");
   SudokuRazy.TitleLoader.animateHeader(title)
   setTimeout(SudokuRazy.WelcomeView.showForms, 2500)
 
   // This does all the steps for the actual game section to run
-  SudokuRazy.GameView.hideLightbox();
-  $('.incorrect').hide()
-  $('#tabs').tabs();
   var controller = new SudokuRazy.GameController(new SudokuRazy.GameModel, SudokuRazy.GameView)
   controller.bindEvents()
 }
