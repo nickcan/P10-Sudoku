d915cbac5040ce7e717c01f4de3fc14e14d5e0ea
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-16 09:30:57 -0700

    Moved the initial steps for running the Welcome message into SudokuRazy.app and scoped the TitleLoader object under SudokuRazy.  Lastly, Changed the spelling of the name to SudokuRazy

--- public/js/SudokuRazy.js
+++ public/js/SudokuRazy.js
@@ -1,6 +1,13 @@
 SudokuRazy = {};
 
 SudokuRazy.app = function() {
+  // For the Welcome page and typing of welcome message
+  SudokuRazy.WelcomeView.hideForms();
+  var title = "Welcome to SudokuRazy".split("");
+  SudokuRazy.TitleLoader.animateHeader(title)
+  setTimeout(SudokuRazy.WelcomeView.showForms, 2500)
+
+  // This does all the steps for the actual game section to run
   SudokuRazy.GameView.hideLightbox();
   $('.incorrect').hide()
   $('#tabs').tabs();
